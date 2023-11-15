debc1dec7dbe228449242bc554c0aa522c223187
ridwaanhall <erbyl14@gmail.com>
2023-11-15 11:08:23 +0700

    String Methods

---
+++ 002_string methods.py
@@ -0,0 +1,16 @@
+text = 'ridwan halim <3 hafidhah afkarianaA'
+
+print(len(text))
+print(text[0])
+print(text[1])
+print(text[2])
+print(text[3])
+print(text[4])
+print(text[5])
+
+print(text.find('Hafidhah'))
+print(text.find('a'))
+print(text.count('a'))
+print(text.capitalize())
+print(text.upper())
+print(text.lower())
