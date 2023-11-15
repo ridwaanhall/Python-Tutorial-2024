d17fa8c569f41eedf8214cdcf1dfb0406c9e266d
ridwaanhall <erbyl14@gmail.com>
2023-11-15 10:54:28 +0700

    variable and string

---
+++ 001_variable.py
@@ -0,0 +1,45 @@
+# variable is a name that can be used to refer to a value
+
+first_name = "Ridwan"
+last_name = "Halim"
+
+full_name = first_name + " " + last_name
+print('first name =',first_name)
+print('last name =',last_name)
+print("full name =",full_name)
+print('type =',type(full_name))
+
+#========================================
+
+age = 21
+print('age =',age)
+age += 1
+print('age =',age)
+print('age str='+str(age))
+print('age type =',type(age))
+
+#========================================
+
+height = 158.5
+print('height ='+str(height))
+print('type =',type(height))
+
+#========================================
+
+is_married = False and True
+print('is_married =',is_married)
+is_married = True and True
+print('is_married =',is_married)
+print('type =',type(is_married))
+
+#========================================
+# multiple assignment
+a = b = c = 10
+print('a =',a)
+print('b =',b)
+print('c =',c)
+
+nama_depan, nama_belakang, nama_lengkap = 'Ridwan', 'Halim', 'Ridwan Halim'
+print('nama depan =',nama_depan)
+print('nama belakang =',nama_belakang)
+print('nama_lengkap =',nama_lengkap)
